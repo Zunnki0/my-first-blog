de32bc7e3de4a421eeb9dd4e18b762701704f020
Ana <ana.alonso.f@hotmail.com>
2018-08-26 10:54:15 +0100

    Added view template and modified others (CSS included) to allow a post comment

--- blog/models.py
+++ blog/models.py
@@ -14,3 +14,25 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')
+    author=models.CharField(max_length=200)
+    text=models.TextField()
+    created_date=models.DateTimeField(default=timezone.now)
+    approved_comment=models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment=True
+        self.save()
+
+    def __str__(self):
+        return self.text
+
+''' Mi opcion antes al crear el model Comment era buscar desde el post en el que estes recoger la pk (xq te la pasa la view al llegar) y asociarlo asi desde el model. No funcionaba x demas fallos
+seguramente mala sintaxis tambn si es q se podria hacer asi. Pero a lo mejor es un poco complicado si implicas que hay una pk en la definicion que solo se muestra cuando se ha creado el comment
+En verdad vendria mas o menos a ser lo mismo ya q en la view lo asgnas asi de todas formas¿? Sería como una forma manual (aun asi una no muy buena opcion)'''
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.utils import timezone
-from .models import Post
+from .models import Post,Comment
 from django.shortcuts import render, get_object_or_404
-from .forms import PostForm
+from .forms import PostForm,CommentForm
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
 
@@ -15,9 +15,36 @@ def post_draft_list(request):
     posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
     return render(request, 'blog/post_draft_list.html', {'posts': posts})
 
-def post_detail(request,pk):
+'''def post_detail(request,pk):                                            #muchas cosas nuevas aquí copiando estructura de nuevo y mezclando con la de post_detail
     post=get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post':post})
+    if request.method=="POST":
+        form=CommentForm(request.POST)
+        if form.is_valid():
+            comment=form.save(commit=False)
+            comment.post=post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form=CommentForm()
+        return render(request, 'blog/post_detail.html', {'post':post}, {'form':form})
+'''
+
+def post_detail(request,pk):                                            #muchas cosas nuevas aquí copiando estructura de nuevo y mezclando con la de post_detail
+    post=get_object_or_404(Post, pk=pk)                                 # por alguna razon no me funciona hacer         comments=post.comments.order_by('created_date')
+    return render(request, 'blog/post_detail.html', {'post':post})      # con                                   ....   ... {'comments':comments}   y en el post_detail {%for comment in comments%}
+
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/add_comment_to_post.html', {'form': form})
 
 @login_required
 def post_new(request):
@@ -26,7 +53,8 @@ def post_new(request):
         if form.is_valid():
             post=form.save(commit=False)
             post.author=request.user
-            post.save()
+            post.published_date = timezone.now()
+            post.save()                                                         #accediendo a la función de la clase Post. Influye en la published_date
             return redirect('post_detail', pk=post.pk)
     else:
         form=PostForm()
@@ -38,7 +66,8 @@ def post_edit(request, pk):
     if request.method == "POST":
         form = PostForm(request.POST, instance=post)
         if form.is_valid():
-            post.author = request.user
+            post = form.save(commit=False)          #por alguna razón el post con el save faltaba antes de que me decidiera a trackear x q faltaba la published_date tambn
+            post.author = request.user               #la publishd date la quitamos de aquí para crear primeor el draft. Y lugo con la post_publish view ya nos encargamos de eso
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -56,3 +85,15 @@ def post_remove(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.delete()
     return redirect('post_list')
+
+@login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk=comment.post.pk)
+
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.delete()
+    return redirect('post_detail', pk=comment.post.pk)
